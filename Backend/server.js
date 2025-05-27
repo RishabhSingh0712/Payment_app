92d870ea128edefa4f5eaa39329fd21349d22eff
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2025-05-27 22:49:24 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,19 @@ import Razorpay from "razorpay";
 
 
 dotenv.config();
+
+//Razorpay configuration
+ export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
+const processPayment = async (req, res) => { 
+  res.status(200).json({
+    success: true,
+  })
+}
+// Starting the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
